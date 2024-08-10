af72732ded5dfbe9a59a4fd59208081615b8b7aa
0xJam3z <0xjam3z@gmail.com>
2024-08-10 17:23:17 -0400

    Number Reversal Sequence

---
+++ printReversedNumber.c
@@ -0,0 +1,14 @@
+// Input 3 numbers. Program will reverse these 
+// three numbers step by step.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int digits, units, tens, hundreds;
+    printf("Input 3 digits: ");
+    scanf("%d", &digits);
+    hundreds = digits / 100;
+    tens = (digits / 10) % 10;
+    units = digits % 10;
+    printf("The inverse of %d is %d%d%d\n", digits, units, tens, hundreds);
+}
