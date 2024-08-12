09a71c1f75a1cd4bdef15498b59147dc34f23bb0
0xJam3z <0xjam3z@gmail.com>
2024-08-12 16:50:56 -0400

    Checks whether a number is even or odd using modulo

---
+++ oddOrEven.c
@@ -0,0 +1,14 @@
+// Checks whether a number is even or odd
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //Code
+    int num;
+    printf("Input a number: ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+        printf("The number %d is even\n", num);
+    else
+        printf("The number %d is odd\n", num);
+}
