c6c9451e4b58dc2dbf560facffa32e9eec96d73d
0xJam3z <0xjam3z@gmail.com>
2024-08-16 12:56:23 -0400

    Grading scale

--- multipleGradingProgram.c
+++ multipleGradingProgram.c
@@ -1,26 +1,17 @@
-/*  Get grade from user. If grade 80+ print "Excellent Job"
-    If grade is less than 80 but higher than 60 print "Not bad"
-    If grade is less than 60 print "You didn't pass."
+/*  Get grade from user. If grade 80+ print "Excellent Job!"
+    If grade is less than 80 but higher than 60 print "Not bad!"
+    If grade is less than 60 print "Oh, you failed."
 */
 
 int main()
 {
-
     float inputGrade;
-
-    printf("Input a Grade: ");
-    scanf("%f", &inputGrade);
-
+    printf("Input your grade: ");
+    scanf("%f", inputGrade);
     if (inputGrade >= 80)
-    {
-        printf("Excellet job!");
-    }
+        printf("Excellent job!");
     else if (inputGrade >= 60)
-    {
         printf("Not bad!");
-    }
     else
-    {
-        printf("You didn't pass.");
-    }
+        printf("Oh, you failed.");
 }
