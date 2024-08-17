3e225627f29de78a92a7cd6199258bf6bae3901a
0xJam3z <0xjam3z@gmail.com>
2024-08-17 15:09:31 -0400

    Grading system using cases

---
+++ gradingSystemCase.c
@@ -0,0 +1,33 @@
+// Grading System Case usage provided by Udemy Course. Uploaded for future reference. Rewritten by 0xJam3z (video didn't include F case.)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    char grade;
+    printf("Enter your grade (A-F): ");
+    scanf("%c", &grade);
+    switch (grade) // Specifying Variable
+    {
+    case 'A':
+        printf("Grade between 90-100 \n");
+        break;
+    case 'B':
+        printf("Grade between 80-89 \n");
+        break;
+    case 'C':
+        printf("Grade between 70-79 \n");
+        break;
+    case 'D':
+        printf("Grade between 60-69 \n");
+        break;
+    case 'F':
+        printf("Grade between 50-59 \n");
+        break;
+    default:
+        print("Not a correct input. \n");
+        break;
+    }
+    return 0;
+}
