308fd1bfc115e45de0c0e2128a0c19540efff15d
0xJam3z <0xjam3z@gmail.com>
2024-08-20 14:32:26 -0400

    Convert an ascii char to hexD/decimal representation

---
+++ asciiToHexDandDecimal.c
@@ -0,0 +1,14 @@
+// Char to ascii decimal / hexadecimal ascii code
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char myChar1 = 'a';
+
+    printf("Character: %c \n", myChar1);
+    printf("Decimal ASCII Code: %d \n", myChar1);
+    printf("Hexadecimal ASCII COde: 0x%X \n", myChar1);
+
+    return 0;
+}
