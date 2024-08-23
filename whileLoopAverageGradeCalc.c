6540d6661519cfe0bfaa55bd3c2feda3aa96437d
0xJam3z <0xjam3z@gmail.com>
2024-08-23 16:40:19 -0400

    Take X num of grades as input and find average of

---
+++ whileLoopAverageGradeCalc.c
@@ -0,0 +1,21 @@
+// Take x number of grades as input until user inputs -1. Find the average of.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int inputGrade, gradeAverage, i;
+    i = 0;
+    gradeAverage = 0;
+    printf("Input a grade and use -1 to stop calculating: ");
+    scanf("%d", &inputGrade);
+    while (inputGrade !=-1) 
+    {
+        gradeAverage = gradeAverage + inputGrade;
+        i++;
+        printf("Input a grade and use -1 to stop calculating: ");
+        scanf("%d", &inputGrade);
+    }
+    
+    printf("The average grade of entered grades is %d \n", gradeAverage/i);
+    return 0;
+}
