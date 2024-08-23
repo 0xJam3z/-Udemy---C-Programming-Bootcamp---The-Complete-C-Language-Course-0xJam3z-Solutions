e611ed360fb42b5e738bc42cb7fefd98c32b7b08
0xJam3z <0xjam3z@gmail.com>
2024-08-23 13:28:48 -0400

    while loops!

---
+++ whileLoopsNumofAsteriks.c
@@ -0,0 +1,17 @@
+// Take a numeric input of how many asteriks a user wants printed to the screen. 
+// Use a while loop to complete this task.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int userInput;
+    printf("Input how many asteriks you'd like printed to screen: ");
+    scanf("%d", &userInput);
+    int count = 1;
+    while (count <= userInput)
+    {
+        printf("*");
+        count +=1;
+    }
+    return 0;
+}
